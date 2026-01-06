3719d64f8922295cd36ecf0cfaf1835f63803119
praveensingh1999 <praveen969500@gmail.com>
2026-01-06 13:37:25 +0530

    intefrated with login , signup flow and lagout fxnality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,87 @@
-
-
-import './App.css'
-import Home from "./pages/Home"
-import { Route, Routes } from 'react-router-dom'
- import Navbar from './components/common/Navbar'
+import { Route, Routes } from "react-router-dom";
+import Error from "./pages/Error";
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/core/Auth/PrivateRoute";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import UpdatePassword from "./pages/UpdatePassword";
+import VerifyEmail from "./pages/VerifyEmail";
+import Home from "./pages/Home";
+import Navbar from "./components/common/Navbar";
+import OpenRoute from "./components/core/Auth/OpenRoute";
+import "./App.css";
 
 function App() {
- 
-console.log("inside app")
   return (
     <div className="w-screen min-h-screen bg-[#0C2B4E] flex flex-col font-inter">
-      <Navbar/>
-    <Routes>
-     <Route path="/" element={<Home/>}/>
-     </Routes>
-   
-    
-    
-     
+      <Navbar />
+
+      {/* ✅ ALL ROUTES MUST BE INSIDE <Routes> */}
+      <Routes>
+        {/* Open routes */}
+        <Route path="/" element={<Home />} />
+
+        <Route
+          path="/signup"
+          element={
+            <OpenRoute>
+              <Signup />
+            </OpenRoute>
+          }
+        />
+
+        <Route
+          path="/login"
+          element={
+            <OpenRoute>
+              <Login />
+            </OpenRoute>
+          }
+        />
+
+        <Route
+          path="/forgot-password"
+          element={
+            <OpenRoute>
+              <ForgotPassword />
+            </OpenRoute>
+          }
+        />
+
+        <Route
+          path="/verify-email"
+          element={
+            <OpenRoute>
+              <VerifyEmail />
+            </OpenRoute>
+          }
+        />
+
+        <Route
+          path="/update-password/:id"
+          element={
+            <OpenRoute>
+              <UpdatePassword />
+            </OpenRoute>
+          }
+        />
+
+        {/* Protected route */}
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+
+        {/* 404 */}
+        <Route path="*" element={<Error />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
